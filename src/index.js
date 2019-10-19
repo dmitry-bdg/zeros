a4dd84be74b69bb9bd555da57ea6a3f573dc5a01
Dmitry Balahonov <balahonov.dmitry@gmail.com>
2019-10-19 22:25:28 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let factInInput = expression.split('*');
+  let baseForZero = [2, 5];
+  let inTwo = 0;
+  let inFive = 0;
+  
+  factInInput.forEach(item => {
+      let checkFact = item.includes('!!') ? 2 : 1;
+      item = parseInt(item);
+          baseForZero.forEach(inpos => {
+          for (let i = item; i >= inpos; i -= checkFact) {
+              if (!(i % inpos)) {
+                  let x = i;
+                  while (!(x % inpos)) {
+                      x /= inpos;
+                      inpos === 2 ? inTwo += 1: inFive += 1;
+                  }
+              }
+          }   
+      });
+  });
+  return inTwo < inFive ? inTwo : inFive;
 }
